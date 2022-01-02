6baeaea7d2b00096b1ae566e22ac76ae5bdef4f2
Justin Phu <me@justinphu.dev>
2022-01-02 11:08:09 +1100

    [blog] Ensure CodeMonkey text is still viewable on mobile
    Previously, the CodeMonkey text would disappear when zooming in or
    viewing on a small screen (mobile). This maintains it always.

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -19,7 +19,7 @@ const LayoutWrapper = ({ children }) => {
                   <Logo className="h-8" />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
